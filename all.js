a80c4328c8671ae3dd99430a8fcb6d4836b04083
ritumahato <ritumahato592002@gmail.com>
2023-11-02 16:45:33 +0530

    Crowdupss website

---
+++ all.js
@@ -0,0 +1,5 @@
+document.querySelector('.burger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+    document.querySelector('.burger').classList.toggle('active');
+});
+
